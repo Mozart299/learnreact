4bae27db683cb77cbc91585f7579ae1e784fa9f2
Peter Cheune <peterkayk@gmail.com>
2022-11-01 12:13:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    //state
+    this.state = {
+      users: []
+    };
+  }
+
+    getUsers() {
+      axios('https://api.randomuser.me/?nat=US&results=5')
+        .then(Response => this.setState({
+          users: Response.data.results
+        })
+      );      
+    }
+
+    componentDidMount(){
+      this.getUsers();
+    }
+
+    render(){
+      return <div className='App'>
+        {this.state.users.map(user=> 
+        <div>
+          <h3>{user.name.first}</h3>
+          <p>{user.email}</p>
+          <hr />
+        </div>
+        )}
+      </div>;
+    }
 }
 
 export default App;
